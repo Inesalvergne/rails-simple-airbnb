bd4026afe8a9a8cb6d4a783c42affa142e20287a
Ines Alvergne <i.alvergne@gmail.com>
2022-04-14 20:56:17 -0400

    adding styling

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,14 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  private
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 end
